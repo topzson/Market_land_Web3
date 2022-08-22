122c9964c878e973248e30388d3e796028b5d708
Rattatammanoon Prawasuk <toprattatammanoon@hotmail.com>
2022-08-22 17:03:17 +0700

    update Walletbar

--- components/providers/web3/index.tsx
+++ components/providers/web3/index.tsx
@@ -1,7 +1,5 @@
-
-
 import { createContext, FunctionComponent, useContext, useEffect, useState } from "react"
-import { createDefaultState, loadContract, Web3State } from "./utils";
+import { createDefaultState, createWeb3State, loadContract, Web3State } from "./utils";
 import { ethers } from "ethers";
 
 
@@ -15,21 +13,27 @@ const Web3Provider: FunctionComponent<Props> = ({children}) => {
 
   useEffect(() => {
     async function initWeb3() {
-      const provider = new ethers.providers.Web3Provider(window.ethereum as any);
-      const contract = await loadContract("NftMarket", provider);
+      try {
+        const provider = new ethers.providers.Web3Provider(window.ethereum as any);
+        const contract =  await loadContract("NftMarket", provider);
 
-
-      setWeb3Api({
-        ethereum: window.ethereum,
-        provider,
-        contract,
-        isLoading: false
-      })
+        setWeb3Api(createWeb3State({
+          ethereum: window.ethereum,
+          provider,
+          contract,
+          isLoading: false
+        }))
+      } catch(e: any) {
+        console.error("Please, install web3 wallet");
+        setWeb3Api((api) => createWeb3State({
+          ...api as any,
+          isLoading: false,
+        }))
+      }
     }
 
     initWeb3();
   }, [])
-
 
   return (
     <Web3Context.Provider value={web3Api}>
@@ -40,6 +44,11 @@ const Web3Provider: FunctionComponent<Props> = ({children}) => {
 
 export function useWeb3() {
   return useContext(Web3Context);
+}
+
+export function useHooks() {
+  const { hooks } = useWeb3();
+  return hooks;
 }
 
 export default Web3Provider;
